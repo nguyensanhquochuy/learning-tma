575097ae757cb75c5aa7a7c3d96cfcf33df53061
Nguyễn Sanh Quốc Huy <wiftrip@gmail.com>
2024-10-04 17:05:51 +0700

    router

--- 30-09/my-app/src/App.js
+++ 30-09/my-app/src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import MyComponent from './components/MyComponent';
-
+import MyComponent from './components/test/MyComponent';
 import React from 'react';
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
-}
+import Header from './components/Header/Header';
+import { Link, Outlet } from 'react-router-dom';
 
-// function App() {
-//   return (
-//     <div>
-//       hello
-//       <MyComponent></MyComponent>
-//     </div>
-//   );
-// }
+
+const App = () => {
+  return (
+    <div>
+      {/* <MyComponent></MyComponent> */}
+
+      <div className='app-container'>
+        <div className='header-container'>
+          <Header />
+        </div>
+        <div className='main-container'>
+          <div className='side-nav-container'>
+
+          </div>
+          <div className='app-content'>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+
+      {/* <div>
+        test link
+        <div>
+          <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+            <Link to="/users">Go to User page</Link >
+          </button>
+          <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+            <Link to="/admins">Go to Admin page</Link >
+          </button>
+        </div>
+      </div> */}
+    </div>
+  );
+}
 
 export default App;
---
+++ 30-09/my-app/src/components/User/User.js
@@ -0,0 +1,9 @@
+const User = () => {
+    return (
+        <div>
+            User Component
+        </div>
+    )
+}
+
+export default User
